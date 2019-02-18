7aa3afb8fc723a6eb3129bedac666b52db76db02
jcc <jcc@DESKTOP-8C0JC9G>
2019-02-19 00:37:32 +0200

    commit2

--- src/firstpr/main.java
+++ src/firstpr/main.java
@@ -2,8 +2,10 @@ package firstpr;
 
 public class main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		// TODO Auto-generated method stub
+		System.out.print("doaa alkhaldi");
 
 	}
 
