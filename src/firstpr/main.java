2b89bb37e02da760da21fb419694abc0a7aab976
jcc <jcc@DESKTOP-8C0JC9G>
2019-02-20 21:55:50 +0200

    commit 3

--- src/firstpr/main.java
+++ src/firstpr/main.java
@@ -1,12 +1,33 @@
+
+
 package firstpr;
 
-public class main {
 
-	public static void mainn(String[] args)
+public class main
+{    
+	int valu;
+	int nprod;
+	 
+	public main()
+	 
+	 {
+		 valu = 0;
+		 nprod = 0;
+	 }
+	
+
+	public int cartval()
 	{
-		 
-		System.out.print("doaa alkhaldi");
-
+		 return valu;
+	}
+	public void insertbook(int bookn,int val)
+	{
+		valu  = valu+val;
+		nprod = nprod+bookn;
+	}
+	public int cartbook()
+	{
+		return nprod;
 	}
 
 }
